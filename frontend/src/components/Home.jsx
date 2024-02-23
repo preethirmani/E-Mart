076f2cf083b5268f0888e7d90093dd8080c8a821
Preethi <writetopreethir@gmail.com>
2024-02-22 17:08:36 -0800

    adding filter options in Homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Form from 'react-bootstrap/Form';
 import { useGetAllProductsQuery } from "../redux/apiSlice";
 import Rating from "./Rating";
 
@@ -6,11 +7,25 @@ import '../index.css';
 import { Link } from "react-router-dom";
 const Home = ({token}) => {
  
-   const { data, error, isLoading } = useGetAllProductsQuery();
-   console.log('data', data ? data : 'lookatloading');
+  const { data, error, isLoading } = useGetAllProductsQuery();
+   
+  const [selectedCategory, setSelectedCategory] = useState('');
+  
 
+   const categories = data ? Array.from(
+    new Set(data.map(d => d.category))
+   ) : null;
+   console.log('categories', categories);
+
+   const handleSelectChange = (e) => {
+   
+    const newValue = e.target.value;
+    setSelectedCategory(newValue);
+    console.log(selectedCategory);
+   }
+   
    if (isLoading) {
-     console.log('isLoading', isLoading);
+    
     return(
        <div>Loading... Products are loading..</div>
     )
@@ -32,36 +47,46 @@ const Home = ({token}) => {
   return (
   <div className="product-container">
     {
-      data && data.map(product => {
-        return (
-              <div className="product-card" key={product.id}>
-                      <div className="product-img-container">   
-                        <Link to={`/product/${product.id}`}>
-                          <img className='product-coverimage' src={product.image}/>  
-                        </Link> 
-                      </div>
-                      <div className="product-info">
-                        <Link to={`/product/${product.id}`}>
-                            <h6>{product.title}</h6>
-                        </Link>
-                      
-                        
-                        <label>${product.price}</label>
-                        <br />
-                        <div>
-                          <Rating 
-                          value = {product.rating.rate}
-                          text = {`${product.rating.count} reviews`}
-                          />
+     
+      data && ( 
+        <>
 
-                        </div>
-                    
-                        
+          <Form.Select 
+              value={selectedCategory} 
+              onChange={handleSelectChange}>
+                <option>Select a category</option>
+              {
+                categories.map(category => {
+                  return <option>{category}</option>
+                })
+              }
+          </Form.Select>
+          {data.map(product => {
+            return (
+                    <div className="product-card" key={product.id}>
+                            <div className="product-img-container">   
+                              <Link to={`/product/${product.id}`}>
+                                <img className='product-coverimage' src={product.image}/>  
+                              </Link> 
+                            </div>
+                            <div className="product-info">
+                              <Link to={`/product/${product.id}`}>
+                                  <h6>{product.title}</h6>
+                              </Link>
+                              <label>${product.price}</label>
+                              <br />
+                              <div>
+                                <Rating 
+                                value = {product.rating.rate}
+                                text = {`${product.rating.count} reviews`}
+                                />
+                              </div>
+                            </div>
                       </div>
-                </div>
-        )
-      })
-    
+             )
+          })}
+        </>
+      )
     }
   </div>
   )
